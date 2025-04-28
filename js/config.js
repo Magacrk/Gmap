a17b8f31430b90ab39f245916f0067a512e77fde
Ayush Sahu <114695041+Magacrk@users.noreply.github.com>
2025-04-29 01:21:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_IZJpym2Gg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2vbkc7kmfeaqa1i4f5fs00l73m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_OxlGgO4Dm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '34fervhsn6l9air8qr4qbg94r1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
